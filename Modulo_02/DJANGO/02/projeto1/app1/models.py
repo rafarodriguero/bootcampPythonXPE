71549249128d231615a18e89f6644e46f952fa7c
Rafael Souza <rafaelrodriguero1@gmail.com>
2024-01-29 16:33:57 -0300

    Commit da nova ferramenta.

--- Modulo_02/DJANGO/02/projeto1/app1/models.py
+++ Modulo_02/DJANGO/02/projeto1/app1/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     nome = models.CharField('nome', max_length=30)
-    telefone = models.BigIntegerField('telefone')
+    telefone = models.IntegerField('telefone')
     email = models.CharField('email', max_length=30)
 
-    def __str__(self):
-        return f'Nome: {self.nome}, Telefone: {self.telefone}, Email: {self.email}'
+    def __str__(self) -> str:
+        return f"Nome: {self.nome}, Telefone: {self.telefone}, Email: {self.email}"
+
+
--- Modulo_02/DJANGO/02/projeto1/app1/urls.py
+++ Modulo_02/DJANGO/02/projeto1/app1/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import index, create
+from .views import index
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('criar/', create, name='criar'),
+    path('',index,name='index'),
 ]
